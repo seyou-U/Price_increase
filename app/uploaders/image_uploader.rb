36f33552fc4d73c8d3657b851f121677374c0d96
seyou-U <rikakorink54t@yahoo.co.jp>
2022-04-16 16:53:44 +0900

    値上げ商品の初期データを追加

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process resize_to_fill: [630, 357,"center"]
+  process resize_to_fill: [1000, 1000]
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
