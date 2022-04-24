a524af3e7a21713ffebde97925504a8b95b4687d
seyou-U <rikakorink54t@yahoo.co.jp>
2022-04-24 23:05:26 +0900

    損失チェックページの追加

---
+++ app/javascript/components/Keisan.jsx
@@ -0,0 +1,64 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class Product extends React.Component {
+  constructor(props) {
+    super(props)
+    this.changeValue1 = this.changeValue1.bind(this)
+    this.state = {
+      value1: 0
+    }
+  }
+
+  changeValue1(newValue) {
+    this.setState({
+      value1: newValue
+    })
+  }
+
+  render () {
+    const newValue1 = this.state.value1
+    const result = this.props.price * parseInt(newValue1) //* 1年間の週の数
+    const onClickCal = () => {
+      if(!isNaN(result)){
+        this.setState({answer: result});
+      }else{
+        this.setState({answer: "数字以外の文字が入力されています"});
+      };
+    };
+
+    return (
+      <>
+        {this.props.price}
+        <p>×</p>
+        <NumericalValue  value={newValue1} valueChange={this.changeValue1} />
+        <button onClick={onClickCal}>
+          計算する
+        </button>
+        <p>{this.state.answer}</p>
+      </>
+    );
+  };
+}
+
+class  NumericalValue extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e) {
+    this.props.valueChange(e.target.value)
+  }
+
+  render () {
+    const newValue = this.props.value
+    return <input type="text" value={newValue} onChange={this.handleChange} />
+  }
+}
+
+Product.propTypes = {
+  price: PropTypes.integer
+}
+
+export default Product
