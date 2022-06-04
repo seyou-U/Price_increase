77c40c524bbe15a97d27ba39841618d322abaaac
seyou-U <rikakorink54t@yahoo.co.jp>
2022-06-04 17:23:15 +0900

    RuboCopの実行結果を基にコードを修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,18 +10,18 @@ class ProductsController < ApplicationController
   end
 
   def search
-    if  params[:search].present? && params[:name].present?
-      product_name_ids = Product.where('name LIKE(?)',"%#{params[:search]}%").pluck(:id)
+    if params[:search].present? && params[:name].present?
+      product_name_ids = Product.where('name LIKE(?)', "%#{params[:search]}%").pluck(:id)
       genre_name_ids = Genre.where("name= ?", params[:name]).pluck(:id)
-      @products =  Product.where("id IN (?) and genre_id IN (?)", product_name_ids, genre_name_ids)
+      @products = Product.where("id IN (?) and genre_id IN (?)", product_name_ids, genre_name_ids)
     elsif params[:search].present? && params[:name].blank?
-      product_name_ids = Product.where('name LIKE(?)',"%#{params[:search]}%").pluck(:id)
+      product_name_ids = Product.where('name LIKE(?)', "%#{params[:search]}%").pluck(:id)
       genre_name_ids = Genre.all.pluck(:id)
-      @products =  Product.where("id IN (?) and genre_id IN (?)", product_name_ids, genre_name_ids)
+      @products = Product.where("id IN (?) and genre_id IN (?)", product_name_ids, genre_name_ids)
     elsif params[:search].blank? && params[:name].present?
       product_name_ids = Product.all.pluck(:id)
       genre_name_ids = Genre.where("name= ?", params[:name]).pluck(:id)
-      @products =  Product.where("id IN (?) and genre_id IN (?)", product_name_ids, genre_name_ids)
+      @products = Product.where("id IN (?) and genre_id IN (?)", product_name_ids, genre_name_ids)
     else
       @products = Product.all
       redirect_to products_path(@product, @count)
