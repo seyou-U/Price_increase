093c9dd4d3e893af1867fefb3e0d185e6df8e686
seyou-U <rikakorink54t@yahoo.co.jp>
2022-06-15 21:27:54 +0900

    コメント機能のバリデーションを追加し、コントローラを一部変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,11 @@ class CommentsController < ApplicationController
     @comment = current_user.comments.new(comment_params)
     if @comment.save
       redirect_back(fallback_location: root_path)
+    elsif @comment.comment_content.blank?
+      flash[:alert] = "コメントを入力してください"
+      redirect_back(fallback_location: root_path)
     else
+      flash[:alert] = "コメントは500文字以内で入力してください"
       redirect_back(fallback_location: root_path)
     end
   end
