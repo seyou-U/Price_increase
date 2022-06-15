fe87f28e95d723456fc6e6d1a71011029554b240
seyou-U <rikakorink54t@yahoo.co.jp>
2022-06-15 20:47:35 +0900

    Productモデルのテストとバリデーションを追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ApplicationRecord
   belongs_to :genre
   has_many :favorites, dependent: :destroy
+
+  validates :name, presence: true
+  validates :high_price, presence: true
   mount_uploader :image, ImageUploader
   def already_favorited?(user)
     favorites.where(user_id: user.id).exists?
