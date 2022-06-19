13a4b125f080164f72177a2ac8d242ea8bb7741d
seyou-U <rikakorink54t@yahoo.co.jp>
2022-06-19 15:34:33 +0900

    インデント等の細かな点を修正

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
-    factory :product do
-      sequence(:name) { |n| "#{n}円値上げした商品" }
-      sequence(:high_price) { |n| "#{n}" }
-    end
+  factory :product do
+    sequence(:name) { |n| "#{n}円値上げした商品" }
+    sequence(:high_price) { |n| "#{n}" }
   end
+end
