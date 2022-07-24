776b2b13ce8cf39548d42f8b2dcfacf2dd64fe97
seyou-U <rikakorink54t@yahoo.co.jp>
2022-07-24 13:58:00 +0900

    circleci上でエラーが生じたためコードを修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,7 +12,6 @@ if Rails.env.production?
       aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
       region: 'ap-northeast-1'
-      path_style: true
     }
   end
 end
